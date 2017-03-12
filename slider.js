05dfedc955ada0d3ba934e89883abce844310517
Perigree <holisticspiral@gmail.com>
2017-03-12 15:49:58 -0400

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,54 @@
+function findHeightSymbol() {
+
+    // figure out the height the user typed
+    heightStr = document.getElementById('height').value;
+
+    // here we convert the string to an int
+    height = parseInt(heightStr);
+
+    //set the range counter equal to the height
+    document.getElementById("rangeHeight").innerHTML = height;
+
+    //get the selceted symbol
+    var symbol = document.getElementById("symbol").value;
+
+    // draw the pyramid with the given height and selected symbol
+    drawPyramid(height, symbol);
+}
+
+function drawPyramid(height, symbol) {
+
+    // print that pyramid!, but first clear out the old one
+    document.getElementById("pyramid").innerHTML = "";
+
+    for (var row = 0; row < height; row++) {
+
+        // figure out number of bricks and spaces
+        var numBricks = row + 2;
+        var numSpaces = height - row - 1;
+
+        // build up a string for this row
+        var rowStr = "";
+        for (var i = 0; i < numSpaces; i++) {
+            if (symbol == "@")
+                var spaceChar = "\xa0\xa0\xa0";  // code for non-breaking space
+            else {
+                spaceChar = "\xa0\xa0";
+            }
+            rowStr += spaceChar
+        }
+        for (var i = 0; i < numBricks; i++) {
+            rowStr += symbol;
+        }
+
+        textElem = document.createTextNode(rowStr);  // have to create Element and also create TextNode
+
+               // create a <p> element with the text inside
+        rowElem = document.createElement("p");
+        rowElem.appendChild(textElem);
+
+               // insert the paragraph as a child of the container <div>
+        document.getElementById("pyramid").appendChild(rowElem);
+
+        }
+}
